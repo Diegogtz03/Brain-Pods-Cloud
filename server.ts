5a1d156c029f3df91833bbb68a573864ea8f5a49
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-10-19 14:28:33 -0700

    Add websockets

--- server.ts
+++ server.ts
@@ -2,16 +2,43 @@ import express from "express";
 import { WebSocketServer } from "ws";
 
 const app = express();
-const port = 8080;
+const port = 8081;
 
-let wsSessions: WebSocket[] = [];
+let clients: { roomId: string; ws: WebSocket }[] = [];
 
-app.get("/new", (req, res) => {
-  // You get ID of the session
-  // const ws = new WebSocketServer({ noServer: true });
-  // wsSessions.push(ws);
-  res.send("New session created");
+const wss = new WebSocketServer({
+  port: parseInt(process.env.WS_PORT || "3100"),
 });
+
+wss.on("connection", (ctx, req) => {
+  clients.push({ roomId: req.url.split("/")[1], ws: ctx });
+});
+
+wss.on("close", (ctx, req) => {
+  clients = clients.filter((client) => client.ws !== ctx);
+});
+
+app.get("/talk/:roomId", (req, res) => {
+  // send message to all websocket with roomId
+  clients
+    .filter((client) => client.roomId === req.params.roomId)
+    .forEach((client) => {
+      client.ws.send(
+        JSON.stringify({ message: "Whats up? roomId: " + req.params.roomId })
+      );
+    });
+});
+
+app.post("/start", (req, res) => {
+  res.send(req.body.roomId);
+});
+
+// app.get("/open/:roomId", (req, res) => {
+//   // connect to websocket
+//   const ws = new WebSocket(
+//     `ws://localhost:${process.env.WS_PORT}/${req.params.roomId}`
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
