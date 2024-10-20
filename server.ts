b5b03b13232709798555a6b47f637c8de6b89430
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-10-19 17:10:45 -0700

    Changes for new socket interaction

--- server.ts
+++ server.ts
@@ -6,52 +6,60 @@ const app = express();
 var expressWs = require("express-ws")(app);
 const port = 8080;
 
-let clients: { roomId: string; ws: WebSocket }[] = [];
+let clients: { podId: string; ws: WebSocket }[] = [];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to brain pod!");
+});
 
 app.ws("/", (ws, req) => {
-  clients.push({ roomId: req.url.split("/.websocket?roomId=")[1], ws: ws });
+  clients.push({ podId: req.url.split("/.websocket?podId=")[1], ws: ws });
 
   ws.on("close", (ctx, req) => {
     clients = clients.filter((client) => client.ws !== ctx);
   });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-app.get("/talk/:roomId", (req, res) => {
-  // send message to all websocket with roomId
-  clients
-    .filter((client) => client.roomId === req.params.roomId)
-    .forEach((client) => {
-      client.ws.send(
-        JSON.stringify({ message: "Whats up? roomId: " + req.params.roomId })
-      );
-    });
-
-  res.send("Message sent");
-});
-
 app.post("/start", (req, res) => {
-  res.send(req.body.roomId);
-});
+  let podId = req.body.podId;
 
-app.get("/open/:roomId", (req, res) => {
-  console.log("connectione");
-  // connect to websocket
-  const ws = new WebSocket(`ws://localhost:8081/?roomId=${req.params.roomId}`);
+  // Get context on pod (embeddings)
 
-  ws.onopen = () => {
-    res.send("Connected");
-    ws.send(JSON.stringify({ message: "Hello from client" }));
-  };
+  // Generate question for pod
 
-  ws.onerror = (err) => {
-    res.send("Error: " + err);
-  };
+  // Register question on db
+
+  // Send question to pod via websockets
+  sendMessageToPods(podId, {
+    type: "open",
+    data: { question: "Whats up?" },
+  });
+
+  // timeout 1:30 minutes
+  // Send end websocket message to client (3s trial)
+  setTimeout(() => {
+    sendMessageToPods(podId, {
+      type: "close",
+      data: {},
+    });
+  }, 3000);
+
+  // analyze responses
+
+  // If bad answers give feedback through chat (loop?)
+
+  // Repeat
+  res.send("Started");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+function sendMessageToPods(podId: string, data: any) {
+  clients
+    .filter((client) => client.podId === podId)
+    .forEach((client) => {
+      client.ws.send(JSON.stringify(data));
+    });
+}
